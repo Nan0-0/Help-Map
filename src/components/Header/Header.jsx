3d51d5b1a15623ab318b1db6dbd5df26fd3eba76
Nan0-0 <145148559+Nan0-0@users.noreply.github.com>
2023-12-14 22:19:56 -0800

    getting the header set up :0

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,32 @@
 import React from "react";
+import { Autocomplete } from "@material-ui/lab";
+import { AppBar,Toolbar, Typography, InputBase, Box } from "@material-ui/core";
+import SearchIcon from "@material-ui/icons/Search"
 
 const Header = () => {
     return (
-        <h1>Header</h1>
+        <AppBar position="static">
+            <Toolbar className= {classes.Toolbar}>
+                <Typography variant="h5" className= {classes.title}>
+                    Help Map 
+                </Typography>
+                <Box display="flex">
+                    <Typography variant="h6" className= {classes.title}>
+                        See More...
+                    </Typography>
+                    <Autocomplete>
+                        <div className= {classes.search}>
+                            <div className={classes.SearchIcon}>
+                                <SearchIcon/>
+                            </div>
+                            <InputBase placeholder= "Search..." classes = {{ root: classes.inputRoot, input: classes.inputInput}}/>
+                        </div>
+                    </Autocomplete>
+
+                </Box>
+
+            </Toolbar>
+        </AppBar> 
     );
 }
 
