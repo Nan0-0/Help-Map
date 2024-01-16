2de3e031c915a2917108290b51e16349b893f569
Nan0-0 <145148559+Nan0-0@users.noreply.github.com>
2024-01-16 10:04:13 -0800

    List attempt #1

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,9 +1,33 @@
-import React from "react";
 
-const List = () => {
-    return (
-        <h1>List</h1>
-    );
-}
+import React from 'react';
+import { Box } from '@material-ui/core';
+
+
+const listStyles = {
+  container: {
+    direction: "column",
+    bg: "whiteAlpha.900",
+    width: "37vw",
+    height: "100vh",
+    position: "absolute",
+    left: 0,
+    top: 0,
+    zIndex: 1,
+    overflow: "hidden",
+    px: 2,
+  },
+};
+
+const List = ({ items, isLoading, renderItem }) => {
+
+
+  return (
+    <Box {...listStyles.container}>
+      <Box flex={1} overflowY={"scroll"} mt={16} display="flex" flexDirection="column">
+        {items && items.map((item, i) => renderItem(item, i))}
+      </Box>
+    </Box>
+  );
+};
 
 export default List;
